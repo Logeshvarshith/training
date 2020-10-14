b745b06ccf5b56561cf5dc7c8599b1b410587ee3
Logesh <logesh1104@gmail.com>
2020-10-14 17:23:47 +0530

    New commit

--- learnsample.java
+++ learnsample.java
@@ -4,21 +4,9 @@ System.out.println("New one");
 System.out.println("New world");
 System.out.println("New world1");
 System.out.println("New world max");
-
-}
-}
-
-
-//commit changes and training purpose
-class name{
-public static void main(String[] args){
 System.out.println("New one");
 System.out.println("New world");
 System.out.println("New world1");
 System.out.println("New world max");
-
 }
 }
-
-
-//commit changes and training purpose
